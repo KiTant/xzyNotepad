7df1e18b3561cd7f2cff56f28557f3369d158ac4
KiTant <111273015+KiTant@users.noreply.github.com>
2024-08-06 23:35:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import customtkinter as ctk
+import os
 from ui.main_window import MainWindow
 
+
+def resource_path(file):
+    data_dir = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(data_dir, file)
+
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("assets/yellow.json")
+try: ctk.set_default_color_theme(resource_path('yellow.json'))
+except: ctk.set_default_color_theme('assets/yellow.json')
 
 if __name__ == '__main__':
-    app = MainWindow()
+    app = MainWindow(resource_path=resource_path)
     app.mainloop()
