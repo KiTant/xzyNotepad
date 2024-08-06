bad2008189c0bdeb177e466d97c079be989088d7
KiTant <111273015+KiTant@users.noreply.github.com>
2024-08-06 23:31:45 +0700

    Update about_window.py

--- ui/about_window.py
+++ ui/about_window.py
@@ -1,14 +1,16 @@
 import customtkinter as ctk
 from utils.helpers import Version
 
+
 class AboutWindow(ctk.CTkToplevel):
-    def __init__(self, MainWindow: ctk.CTk):
+    def __init__(self, MainWindow: ctk.CTk, resource_path):
         super().__init__()
         self.MainWindow = MainWindow
 
         self.title("About")
         self.geometry("300x200")
-        self.iconbitmap('assets/xzy-notepad-icon.ico')
+        try: self.iconbitmap(resource_path('xzy-notepad-icon.ico'))
+        except: self.iconbitmap('assets/xzy-notepad-icon.ico')
 
         about_text = f"xzyNotepad\nVersion {Version}\n\nAn open-source versatile notepad \ndesigned specifically for writing and editing code \nin various programming languages."
         ctk.CTkLabel(self, text=about_text, justify="center").pack(expand=True)
