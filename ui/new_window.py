29ef4d89f5c167865346a90ce9c3963d9c40ba2b
KiTant <111273015+KiTant@users.noreply.github.com>
2024-08-31 22:32:00 +0700

    Update new_window.py

--- ui/new_window.py
+++ ui/new_window.py
@@ -1,38 +1,41 @@
 import customtkinter as ctk
 from ui.title_menu import TitleMenu
 from CTkCodeBox import CTkCodeBox
+from utils.helpers import code_window_binder, close_code_window, save_file
+from utils.variables import LANGUAGES
 
 
 class NewWindow(ctk.CTkToplevel):
     def __init__(self, MainWindow: ctk.CTk, resource_path):
         super().__init__()
         self.MainWindow = MainWindow
-        self.title("xzyNotepad - New File.py")
+        self.current_language = "txt"
+        self.title(f"xzyNotepad - New File.{self.current_language}")
         try: self.iconbitmap(resource_path('xzy-notepad-icon.ico'))
         except: self.iconbitmap('assets/xzy-notepad-icon.ico')
         self.geometry("1000x500")
         self.minsize(1000, 350)
         self.maxsize(1250, 1000)
 
-        self.codebox = CTkCodeBox(self, language="python", height=500, font=ctk.CTkFont(family=MainWindow.font_family, size=MainWindow.font_size))
-        self.codebox.pack(fill="both", expand=True)
-        self.codebox.bind("<Tab>", MainWindow.insert_tab)
-        self.codebox.bind("<<Modified>>", lambda event: MainWindow.code_updated(self, event))
-        self.bind("<Control-s>", lambda event: MainWindow.save_as_file(self, self.codebox, self.current_language))
-        self.bind("<Control-p>", lambda event: MainWindow.preferences())
-        self.bind("<Control-o>", lambda event: MainWindow.open_file(self, self.codebox, self.saved))
-        self.bind("<Control-n>", lambda event: MainWindow.new_file())
-
-        self.file_name = "New File.py"
+        self.file_name = f"New File.{self.current_language}"
         self.saved = True
-        self.current_language = "py"
+        self.full_file_path = None
+
+        self._initialize_components()
+
+    def _initialize_components(self):
+        self.codebox = CTkCodeBox(self, language=LANGUAGES[self.current_language], height=500, theme=self.MainWindow.settings["codebox_theme"].lower(),
+                                  font=ctk.CTkFont(family=self.MainWindow.settings["font_family"], size=self.MainWindow.settings["font_size"]))
+        self.codebox.pack(fill="both", expand=True)
+        code_window_binder(self.MainWindow, self)
         self.menu = TitleMenu(self.MainWindow, self)
 
         self.protocol("WM_DELETE_WINDOW", self.activate_destroy)
-        MainWindow.all_children.append(self)
+        self.MainWindow.all_children.append(self)
 
         self.after(100, lambda: self.focus_set())
+        self.after(self.MainWindow.settings["auto_save_file_time"] * 60000, lambda: save_file(self.MainWindow, self, self.full_file_path, True))
 
     def winfo_name(self): return '!toplevel'
 
-    def activate_destroy(self): self.MainWindow.do_quit(self, self.saved)
+    def activate_destroy(self): close_code_window(self, self.MainWindow.all_children)
