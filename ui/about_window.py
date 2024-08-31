4a0dda1f6feee95c0ab2a1315b104c73a56e5cfe
KiTant <111273015+KiTant@users.noreply.github.com>
2024-08-31 22:22:10 +0700

    Update about_window.py

--- ui/about_window.py
+++ ui/about_window.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-from utils.helpers import Version
+from utils.helpers import close_window
+from utils.variables import VERSION
 
 
 class AboutWindow(ctk.CTkToplevel):
@@ -12,9 +13,9 @@ class AboutWindow(ctk.CTkToplevel):
         try: self.iconbitmap(resource_path('xzy-notepad-icon.ico'))
         except: self.iconbitmap('assets/xzy-notepad-icon.ico')
 
-        about_text = f"xzyNotepad\nVersion {Version}\n\nAn open-source versatile notepad \ndesigned specifically for writing and editing code \nin various programming languages."
+        about_text = f"xzyNotepad\nVersion {VERSION}\n\nAn open-source versatile notepad \ndesigned specifically for writing and editing code \nin various programming languages."
         ctk.CTkLabel(self, text=about_text, justify="center").pack(expand=True)
         MainWindow.all_children.append(self)
-        self.protocol("WM_DELETE_WINDOW", lambda: MainWindow.destroy_other_window(self))
+        self.protocol("WM_DELETE_WINDOW", lambda: close_window(self, self.MainWindow))
 
         self.after(100, lambda: self.focus_set())
